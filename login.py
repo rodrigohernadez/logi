40dbbe1fe4df537503761159b61fd92908ee94b3
TE HACKER <ro.dz.p12@gmail.com>
2020-03-18 09:32:18 -0600

    Update login.py
    hasta aqui mi reporte Joaquin

---
+++ login.py
@@ -0,0 +1,18 @@
+from tkinter import *
+ventana = Tk()
+ventana.config(bg="#3CB371")
+miFrame = Frame(ventana, width=200,heigh=200)
+heigth=200
+miFrame.pack()
+la1= Label(miFrame, text = "usuario:")
+la1.grid(row=0, column=0, pady=10, padx=10)
+la2= Label(miFrame, text="Pasword")
+la2.grid(row=1,column=0, pady=10, padx=10)
+txt1 = Entry(miFrame, justify= "center")
+txt1.grid(row=0,column=1, pady=10, padx=10)
+txt2 = Entry(miFrame, show="#")
+txt2.grid(row=1,column=1, pady=10, padx=10)
+btnAceptar= Button(miFrame, text="Aceptar")
+btnAceptar.grid(row=2,column=0, pady=10, padx=10, columnspan=2)
+btnAceptar.config(width=30)
+ventana.mainloop()
